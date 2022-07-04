5e6ac3ad73913a870c678cb96f1a40707b8d6ed2
Phan Ba Huy <huy.pb.2011@gmail.com>
2022-07-04 10:54:52 +0700

    Upload

---
+++ assets/script/main.js
@@ -0,0 +1,31 @@
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+const headerBtn = $(".header-menu__icon");
+const overplay = $(".overlay");
+const headerMobile = $(".header-mobile");
+const closeBtn = $(".header-mobile__close");
+
+const app = {
+  handleEvents: function () {
+    headerBtn.onclick = function () {
+      overplay.classList.add("enable");
+      headerMobile.classList.add("enable");
+
+      overplay.classList.remove("disable");
+      headerMobile.classList.remove("disable");
+      // console.log(overplay.classList);
+    };
+    closeBtn.onclick = function () {
+      overplay.classList.add("disable");
+      headerMobile.classList.add("disable");
+
+      overplay.classList.remove("enable");
+      headerMobile.classList.remove("enable");
+    };
+  },
+  start: function () {
+    this.handleEvents();
+  },
+};
+app.start();
